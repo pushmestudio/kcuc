b955b39da682893adb3fe44a719ccd0e3918b4da
tkhm <devtkhm@gmail.com>
2016-11-03 01:53:21 +0900

    Fix broken lint and add another components

--- src/main/webapp/WebContent/js/components/App.js
+++ src/main/webapp/WebContent/js/components/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import Footer from './Footer'
-import AddTodo from '../containers/AddTodo'
-import VisibleTodoList from '../containers/VisibleTodoList'
+import React from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
+import Popup from '../containers/Popup';
 
 const App = () => (
   <div>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
+    <Popup />
   </div>
-)
+);
 
-export default App
+export default App;
