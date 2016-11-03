23900a67ae78f310cf47707f45b63cf3cadbf4f8
tkhm <no-reply@tkhm.dev>
2016-11-03 17:15:28 +0900

    Add sample module and client-side intro

--- src/main/webapp/WebContent/js/reducers/popupReducer.js
+++ src/main/webapp/WebContent/js/reducers/popupReducer.js
@@ -1,10 +1,12 @@
 const popupReducer = (state = {isClicked : false}, action) => {
   switch (action.type) {
   case 'SHOW_POPUP':
-    alert('on reducer' + action.text + ':' + action.count);
+    alert('on reducer ' + action.text + ' : ' + action.count);
     console.log('previous state is :' + state.isClicked);
-    state.isClicked = !(state.isClicked);
-    return state;
+
+    return {
+      isClicked: !state.isClicked
+    };
 
   default:
     return state;
