b35dfeeecf380e9ee87f8cf516792e112671f22f
tkhm <no-reply@tkhm.dev>
2016-11-06 22:22:36 +0900

    Add page-key based update check

--- src/main/java/jp/pushmestudio/kcuc/dao/UserInfoDao.java
+++ src/main/java/jp/pushmestudio/kcuc/dao/UserInfoDao.java
@@ -4,19 +4,52 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import jp.pushmestudio.kcuc.model.UserInfo;
 
 public class UserInfoDao {
 	/**
-	 * @return DBから取得したデータ
+	 * 指定したIDのユーザーを返す
+	 * 
+	 * @param searchId
+	 *            探す対象となるユーザーのID
+	 * @return DBから取得した、IDに該当するユーザー
 	 */
-	public List<UserInfo> getUserList() {
+	public List<UserInfo> getUserList(String searchId) {
 		List<UserInfo> userList = new ArrayList<>();
 
 		// TODO ここでDBに対してselect処理などを実施する、今はダミーの値を使用する
 		DummyStatement stmt = new DummyStatement();
-		List<List<Object>> rs = stmt.executeQuery(1);
+		List<List<Object>> rs = stmt.executeQueryUser(searchId);
+
+		for (int i = 0; i < rs.size(); i++) {
+			List<Object> gotUser = rs.get(i);
+			String id = (String) gotUser.get(0);
+			String password = (String) gotUser.get(1);
+			@SuppressWarnings("unchecked")
+			Map<String, Long> subscribedPages = (Map<String, Long>) gotUser.get(2);
+
+			UserInfo userInfo = new UserInfo(id, password, subscribedPages);
+			userList.add(userInfo);
+		}
+
+		return userList;
+	}
+
+	/**
+	 * 指定したページを購読しているユーザーを返す
+	 * 
+	 * @param pageHref
+	 *            購読ページ
+	 * @return 指定した購読ページを購読しているユーザー
+	 */
+	public List<UserInfo> getSubscribedUserList(String pageHref) {
+		List<UserInfo> userList = new ArrayList<>();
+
+		// TODO ここでDBに対してselect処理などを実施する、今はダミーの値を使用する
+		DummyStatement stmt = new DummyStatement();
+		List<List<Object>> rs = stmt.executeQueryPage(pageHref);
 
 		for (int i = 0; i < rs.size(); i++) {
 			List<Object> gotUser = rs.get(i);
@@ -34,16 +67,42 @@ public class UserInfoDao {
 
 	class DummyStatement {
 		// DBからデータを取得することを擬制するために、executeQueryの名前で取得
-		// 引数の値で、テーブルから取得する件数を指定できるようにしている
-		public List<List<Object>> executeQuery(int getRequestCount) {
+		// 引数の値で、ユーザーIDが合致するもののみ返す
+		public List<List<Object>> executeQueryUser(String searchId) {
+			Objects.requireNonNull(searchId);
 			List<List<Object>> dummyResultSet = new ArrayList<>();
 
 			List<List<Object>> dummyUserTable = new DummyUserTable().getUserTableData();
-			// 取得依頼件数がダミーテーブルの件数よりも多く指定された場合は、ダミーテーブルの件数を取得依頼件数とする
-			getRequestCount = dummyUserTable.size() < getRequestCount ? dummyUserTable.size() : getRequestCount;
-			for (int i = 0; i < getRequestCount; i++) {
+			for (int i = 0; i < dummyUserTable.size(); i++) {
 				List<Object> dummyData = dummyUserTable.get(i);
-				dummyResultSet.add(dummyData);
+
+				// dummyData.get(0) = ユーザーID
+				if (searchId.equals((String) dummyData.get(0))) {
+					dummyResultSet.add(dummyData);
+				}
+			}
+
+			return dummyResultSet;
+		}
+
+		public List<List<Object>> executeQueryPage(String pageHref) {
+			Objects.requireNonNull(pageHref);
+			List<List<Object>> dummyResultSet = new ArrayList<>();
+
+			List<List<Object>> dummyUserTable = new DummyUserTable().getUserTableData();
+			for (int i = 0; i < dummyUserTable.size(); i++) {
+				List<Object> dummyData = dummyUserTable.get(i);
+
+				// dummyData.get(2) = 購読中のページと最後に確認した時間
+				@SuppressWarnings("unchecked")
+				HashMap<String, Long> subscribedPages = (HashMap<String, Long>) dummyData.get(2);
+
+				// 購読ページ一覧の中にpageHrefがないユーザーはリストに足さない
+				if (Objects.isNull(subscribedPages.get(pageHref))) {
+					continue;
+				} else {
+					dummyResultSet.add(dummyData);
+				}
 			}
 
 			return dummyResultSet;
@@ -59,7 +118,7 @@ public class UserInfoDao {
 			String dummy1Id = "capsmalt";
 			String dummy1Password = "pass";
 			Map<String, Long> dummy1SubscribedPages = new HashMap<>();
-			dummy1SubscribedPages.put("SSAW57_liberty", 1469114812137L);
+			dummy1SubscribedPages.put("SSAW57_liberty/com.ibm.websphere.wlp.nd.doc/ae/cwlp_about.html", 1469114812137L);
 			dummyUser1.add(dummy1Id);
 			dummyUser1.add(dummy1Password);
 			dummyUser1.add(dummy1SubscribedPages);
--- src/test/java/jp/pushmestudio/kcuc/model/KCDataTest.java
+++ src/test/java/jp/pushmestudio/kcuc/model/KCDataTest.java
@@ -2,22 +2,26 @@ package jp.pushmestudio.kcuc.model;
 
 import static org.junit.Assert.assertTrue;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
 public class KCDataTest {
 
 	@Test
-	public void 特定製品の最終更新日付を取得して事前に定めた更新日付けよりも新しいことを確認できる() {
+	public void 特定ユーザーの最終更新日付よりもKCの最終更新日付けの方が新しいことを確認できる() {
 		// setup
 		KCData data = new KCData();
 		String hrefKey = "SSAW57_liberty/com.ibm.websphere.wlp.nd.doc/ae/cwlp_about.html";
 
 		// execute
 		JSONObject checkResult = data.checkPageUpdate(hrefKey);
+		// System.out.println(checkResult); 
 
 		// verify
-		Boolean actual = (Boolean) checkResult.getBoolean("isUpdated");
+		JSONArray userList = checkResult.getJSONArray("userList");
+		JSONObject firstUser = userList.getJSONObject(0);
+		boolean actual = firstUser.getBoolean("isUpdated");
 		assertTrue(actual);
 	}
 
