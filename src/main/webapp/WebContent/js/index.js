fc0c9ba2200e19f623bbddedd0c2c6dca07f4186
tkhm <devtkhm@gmail.com>
2016-11-17 16:34:06 +0900

    Add redux-thunk as middleware

--- src/main/webapp/WebContent/js/index.js
+++ src/main/webapp/WebContent/js/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import todoApp from './reducers';
 import App from './components/App';
 
-let store = createStore(todoApp);
+let store = createStore(
+  todoApp, applyMiddleware(thunk)
+);
 
 render(
   <Provider store={store}>
