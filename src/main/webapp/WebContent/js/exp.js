26453a789ba8f6fc006daa90413bc9b5880d9077
tkhm <no-reply@tkhm.dev>
2016-12-07 13:27:00 +0900

    Update react try v3
    - Split components
    - Update syntax based on ES6

--- src/main/webapp/WebContent/js/exp.js
+++ src/main/webapp/WebContent/js/exp.js
@@ -1,69 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RecentChangesTable from './components/RecentChangesTable';
-
-// Heading component
-RecentChangesTable.Heading = React.createClass({
-  render: function() {
-    return <th>{this.props.heading}</th>;
-  }
-});
-
-// Headings component、渡された数だけ Heading componentを呼び出し
-RecentChangesTable.Headings = React.createClass({
-  render: function() {
-    let headings = this.props.headings.map(function(name) {
-      return <RecentChangesTable.Heading heading = {name} key = {name}/>;
-    });
-    return <thead><tr>{headings}</tr></thead>;
-  }
-});
-
-// Row component
-RecentChangesTable.Row = React.createClass({
-  render: function() {
-    return <tr>
-    <td>{this.props.changeSet.when}</td>
-    <td>{this.props.changeSet.who}</td>
-    <td>{this.props.changeSet.description}</td>
-    </tr>;
-  }
-});
-
-// Rows component, 渡されたデータの数だけRow componentを呼び出し
-RecentChangesTable.Rows = React.createClass({
-  render: function() {
-    let rows = this.props.changeSets.map(function(changeSet) {
-      return(<RecentChangesTable.Row changeSet = {changeSet} key = {changeSet.id}/>);
-    });
-    return <tbody>{rows}</tbody>;
-  }
-});
+import Headings from './components/Headings';
+import Rows from './components/Rows';
 
 // App component, Headings/Rows componentを呼び出し
-let App = React.createClass({
-  render: function() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      headings : props.headings,
+      changeSets : props.changeSets
+    };
+  }
+
+  render() {
+    console.log('exp');
+    console.dir(this.props);
+    console.dir(this.state);
     return <RecentChangesTable className = 'table'>
-    <RecentChangesTable.Headings headings = {this.props.headings} />
-    <RecentChangesTable.Rows changeSets = {this.props.changeSets} />
+    <Headings headings = {this.state.headings} />
+    <Rows changeSets = {this.state.changeSets} />
     </RecentChangesTable>;
   }
-});
+}
 
 let data = [{
-  'id': 1,
   'when': '2 minutes ago',
   'who': 'Jill Dupre',
   'description': 'Created new account'
 },{
-  'id': 2,
   'when': '1 hour go',
   'who': 'Lose White',
   'description': 'Added fist chapter'
 }];
 let headings = ['When', 'Who', 'Description'];
 
+let props = { headings: headings, changeSets: data };
+
 // headings = {headings}で規定した値が Appのprops.headingsになる
-ReactDOM.render(<App headings = {headings}
-  changeSets = {data} />,
-  document.getElementById('root'));
+// {...props}の書き方はReact独自ではなくES6で加わったもの
+ReactDOM.render(<App {...props} />, document.getElementById('root'));
