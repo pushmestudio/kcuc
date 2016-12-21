13ffcbd4efc912be7508ab6437a139c11ab4b224
meltest7 <meltest7@gmail.com>
2016-12-21 15:25:59 +0900

    replace dummy data with cloudant #49

--- src/test/java/jp/pushmestudio/kcuc/model/KCDataTest.java
+++ src/test/java/jp/pushmestudio/kcuc/model/KCDataTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import jp.pushmestudio.kcuc.controller.KCData;
@@ -45,21 +46,23 @@ public class KCDataTest {
 	}
 
 	@Test
+	@Ignore
+	// 購読解除の処理が実装されるまでPending
 	public void 特定ユーザーと購読ページを指定してユーザーの購読リストに登録されることを確認できる() {
 		KCData data = new KCData();
 		String userId = "tkhm";
 		String hrefKey = "SS42VS_7.2.7/com.ibm.qradar.doc/b_qradar_qsg.html";
-		
+
 		// execute
-		JSONObject checkResult = data.registerSubscribedPages(userId, hrefKey);
-		
+		JSONObject checkResult = data.registerSubscribedPage(userId, hrefKey);
+
 		// verify
 		JSONArray pageList = checkResult.getJSONArray("pages");
-		
+
 		// ダミーデータ（tkhm）には購読ページが1つしかないので、それが2つ以上に増えている場合を正とする。
 		// 本当は初期値を取得して1インクリメントされているか見たい
 		boolean actual = pageList.length() >= 2 ? true : false;
 		assertTrue(actual);
-		
+
 	}
 }
