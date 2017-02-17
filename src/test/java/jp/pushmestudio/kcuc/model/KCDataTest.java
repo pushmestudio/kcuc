df0dba3b652bed21f8fcbe4b2d1d2e61c4b56ea1
tkhm <devtkhm@gmail.com>
2017-02-17 19:35:37 +0900

    Remove test ignore and update registration test

--- src/test/java/jp/pushmestudio/kcuc/model/KCDataTest.java
+++ src/test/java/jp/pushmestudio/kcuc/model/KCDataTest.java
@@ -1,11 +1,10 @@
 package jp.pushmestudio.kcuc.model;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.*;
 
 import java.util.List;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import jp.pushmestudio.kcuc.controller.KCData;
@@ -48,9 +47,13 @@ public class KCDataTest {
 	@Test
 	// 購読解除の処理が実装されるまでPending
 	public void 特定ユーザーと購読ページを指定してユーザーの購読リストに登録されることを確認できる() {
+		// setup
 		KCData data = new KCData();
 		String userId = "tkhm";
 		String hrefKey = "SSYRPW_9.0.1/UsingVerseMobile.html";
+
+		// テストデータが事前に登録されていない状態にする
+		data.deleteSubscribedPage(userId, hrefKey);
 
 		Result preResult = data.checkUpdateByUser(userId);
 		List<SubscribedPage> prePageList = ((ResultPageList) preResult).getSubscribedPages();
@@ -61,16 +64,16 @@ public class KCDataTest {
 		// verify
 		List<SubscribedPage> pageList = ((ResultPageList) checkResult).getSubscribedPages();
 
-		// ダミーデータ（tkhm）には購読ページが1つしかないので、それが2つ以上に増えている場合を正とする。
-		// 本当は初期値を取得して1インクリメントされているか見たい
 		boolean actual = pageList.size() > prePageList.size() ? true : false;
 
+		// teardown
 		/*
-		 * TODO ここに今回登録したものの購読解除の処理を書く、なお、本来は@Afterによって実施すべき処理だが、
-		 * 全テストに影響が生じることを踏まえこのテスト内に書くようにする
-		 * なお、assertTrueの後に書いてしまうと、万が一テストの結果が失敗だった際にAssertionErrorが投げられてしまい、
-		 * 購読解除が実現されないので、記載場所には要注意
+		 * テストデータとして登録したページを削除する,
+		 * 本来はteardownメソッド内に書くが現時点ではこの削除処理を必要としていないテストケースが多いため割愛
+		 * assertTrueよりも後に書くと、assertionErrorになったときに削除されずに終わってしまうので注意
 		 */
+		data.deleteSubscribedPage(userId, hrefKey);
+
 		assertTrue(actual);
 	}
 
@@ -89,9 +92,9 @@ public class KCDataTest {
 		// verify
 		int expectedOffset = offset;
 		int expectedNext = offset + limit;
-		int actualOffset = ((ResultSearchList)searchResult).getOffset();
-		int actualNext = ((ResultSearchList)searchResult).getNext();
-		int actualTopicSize = ((ResultSearchList)searchResult).getTopics().size();
+		int actualOffset = ((ResultSearchList) searchResult).getOffset();
+		int actualNext = ((ResultSearchList) searchResult).getNext();
+		int actualTopicSize = ((ResultSearchList) searchResult).getTopics().size();
 
 		assertEquals(expectedOffset, actualOffset);
 		assertEquals(expectedNext, actualNext);
