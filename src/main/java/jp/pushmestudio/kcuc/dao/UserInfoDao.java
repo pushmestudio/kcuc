1b3c8aef0a6b70ef10c41739f66aa7ffc0a121db
tkhm <devtkhm@gmail.com>
2017-03-17 15:07:56 +0900

    Add getting page with product argument, wip

--- src/main/java/jp/pushmestudio/kcuc/dao/UserInfoDao.java
+++ src/main/java/jp/pushmestudio/kcuc/dao/UserInfoDao.java
@@ -75,20 +75,34 @@ public class UserInfoDao {
 	// READ - CRUD
 
 	/**
-	 * 指定したIDのユーザーを返す
+	 * 指定したIDのユーザーとその購読ページを返す
 	 * 
 	 * @param userId
 	 *            探す対象となるユーザーのID
 	 * @return DBから取得した、IDに該当するユーザーの情報
 	 */
 	public List<UserDocument> getUserList(String userId) {
-		/*
-		 * 20161222 接続先をCloudantに移行 public List<UserInfo> getUserList(String
-		 * searchId) { List<UserInfo> userList = new ArrayList<>();
-		 */
-
 		// userIdのインデックスを使用して、指定されたユーザ名に一致するユーザのデータを取得
 		List<UserDocument> userDocs = kcucDB.findByIndex("{\"selector\":{\"userId\":\"" + userId + "\"}}",
+				UserDocument.class);
+
+		return userDocs;
+	}
+
+	/**
+	 * 指定したIDのユーザーとその購読ページを返す
+	 * 
+	 * @param userId
+	 *            探す対象となるユーザーのID
+	 * @param prodId
+	 *            探す対象となる製品のID
+	 * @return DBから取得した、IDに該当するユーザーの、特定製品に限定したページ情報
+	 */
+	public List<UserDocument> getUserList(String userId, String prodId) {
+		// userIdのインデックスを使用して、指定されたユーザ名に一致するユーザのデータを取得
+		List<UserDocument> userDocs = kcucDB.findByIndex(
+				"{\"selector\":{\"$and\":[{\"userId\":\"" + userId
+						+ "\"},{\"subscribedPages\":{\"$elemMatch\":{\"prodId\":\"" + prodId + "\"}}}]}}",
 				UserDocument.class);
 
 		return userDocs;
@@ -118,8 +132,7 @@ public class UserInfoDao {
 	 * @return True or False
 	 */
 	public boolean isUserExist(String userId) {
-		List<UserDocument> userDocs = kcucDB.findByIndex("{\"selector\":{\"userId\":\"" + userId + "\"}}",
-				UserDocument.class);
+		List<UserDocument> userDocs = this.getUserList(userId);
 		return userDocs.size() > 0 ? true : false;
 	}
 
@@ -154,8 +167,7 @@ public class UserInfoDao {
 	public List<UserDocument> setSubscribedPages(String userId, String pageHref, String pageName, String prodId,
 			String prodName) {
 		// useNameのインデックスを使用して、指定されたユーザのデータを取得
-		List<UserDocument> userDocs = kcucDB.findByIndex("{\"selector\":{\"userId\":\"" + userId + "\"}}",
-				UserDocument.class);
+		List<UserDocument> userDocs = this.getUserList(userId);
 
 		// 追加するページの情報を作成
 		Date currentTime = new Date();
@@ -173,8 +185,7 @@ public class UserInfoDao {
 		// UserDocument updatedInfo = kcucDB.find(UserDocument.class,
 		// responseUpdate.getId());
 
-		List<UserDocument> updatedInfo = kcucDB.findByIndex("{\"selector\":{\"userId\":\"" + userId + "\"}}",
-				UserDocument.class);
+		List<UserDocument> updatedInfo = this.getUserList(userId);
 
 		return updatedInfo;
 	}
@@ -211,8 +222,7 @@ public class UserInfoDao {
 		kcucDB.update(updateTarget);
 
 		// 更新後の購読ページ情報をCloudantから取得
-		List<UserDocument> updatedInfo = kcucDB.findByIndex("{\"selector\":{\"userId\":\"" + userId + "\"}}",
-				UserDocument.class);
+		List<UserDocument> updatedInfo = this.getUserList(userId);
 		return updatedInfo;
 	}
 
@@ -227,10 +237,7 @@ public class UserInfoDao {
 	 */
 	public List<UserDocument> cancelSubscribedProduct(String userId, String prodId) throws IndexOutOfBoundsException {
 		// userIdとpageHrefで指定されたユーザのデータを取得
-		List<UserDocument> userDocs = kcucDB.findByIndex(
-				"{\"selector\":{\"$and\":[{\"userId\":\"" + userId
-						+ "\"},{\"subscribedPages\":{\"$elemMatch\":{\"prodId\":\"" + prodId + "\"}}}]}}",
-				UserDocument.class);
+		List<UserDocument> userDocs = this.getUserList(userId, prodId);
 
 		// 指定したユーザが購読中のページ内で，解除対象ページの配列番号を調べる
 		UserDocument updateTarget = kcucDB.find(UserDocument.class, userDocs.get(0).getId());
@@ -259,8 +266,7 @@ public class UserInfoDao {
 		kcucDB.update(updateTarget);
 
 		// 更新後の購読ページ情報をCloudantから取得
-		List<UserDocument> updatedInfo = kcucDB.findByIndex("{\"selector\":{\"userId\":\"" + userId + "\"}}",
-				UserDocument.class);
+		List<UserDocument> updatedInfo = this.getUserList(userId);
 		return updatedInfo;
 	}
 
