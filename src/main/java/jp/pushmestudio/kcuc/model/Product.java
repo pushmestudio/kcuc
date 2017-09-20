01545683546b88baccab00f4e69b762cf4d4848a
tkhm <no-reply@tkhm.dev>
2017-09-20 14:05:48 +0900

    Change res parameter href to pageHref and prodId

--- src/main/java/jp/pushmestudio/kcuc/model/Product.java
+++ src/main/java/jp/pushmestudio/kcuc/model/Product.java
@@ -2,31 +2,36 @@ package jp.pushmestudio.kcuc.model;
 
 import org.json.JSONObject;
 
+/**
+ * 元々のProductはhrefだがパラメーターと応答の整合性からprodIdにしている
+ * ほぼ同じ内容に <{@link TopicProduct}>がある
+ */
+
 public class Product {
-	private String href;
+	private String prodId;
 	private String label;
 
 	public Product() {
-		this.href = "";
+		this.prodId = "";
 		this.label = "";
 	}
-	
-	public Product(String href, String label) {
-		this.href = href;
+
+	public Product(String prodId, String label) {
+		this.prodId = prodId;
 		this.label = label;
 	}
 
 	public Product(JSONObject jsonProduct) {
-		this.href = jsonProduct.getString("href");
+		this.prodId = jsonProduct.getString("href"); // KCの応答はhrefとしてprodIdが入ってくる
 		this.label = jsonProduct.getString("label");
 	}
 
-	public String getHref() {
-		return href;
+	public String getProdId() {
+		return prodId;
 	}
 
-	public void setHref(String href) {
-		this.href = href;
+	public void setProdId(String prodId) {
+		this.prodId = prodId;
 	}
 
 	public String getLabel() {
